96fb17bd6128a849f44872d38920971c41e2b958
Daniel Linkov <daniel_linkov@workmail.com>
2023-10-30 13:10:28 +0200

    Exclusiveness of title editing

--- src/TaskItemRowComponent.js
+++ src/TaskItemRowComponent.js
@@ -4,19 +4,31 @@ export default {
 	components: {
 		'labeled-checkbox': CheckboxComponent,
 	},
+	inject: [ 'editedTaskId' ],
 	props: [ 'task' ],
 	data(){
 		return {
 			editing: false,
 		}
 	},
+	watch: {
+		editing(val){
+			if(val){
+				this.$emit('update:editing',this.task.id);
+			}
+		},
+		editedTaskId(val){
+			if(val != this.task.id){
+				this.editing = false;
+			}
+		}
+	},
 	methods: {
 		onUpdateEditing(val){
 			if(typeof val == 'string'){
-				this.editing = false;
 				this.task.title = val;
-			}else
-				this.editing = false;
+			}
+			this.editing = false;
 		}
 	},
 	template: /* html */`
@@ -29,7 +41,7 @@ export default {
 				@update:editing="onUpdateEditing"
 				>{{ task.title }}</labeled-checkbox>
 			<div class="btn-group">
-				<button class="btn btn-secondary" v-if="!editing" @click="this.editing = true"><i class="bi bi-pencil"></i></button>
+				<button class="btn btn-secondary" title="Edit task" v-if="!editing" @click="this.editing = true"><i class="bi bi-pencil"></i></button>
 				<button class="btn btn-link" v-if="editing" @click="this.editing = false">cancel</button>
 				<button type="button" class="btn btn-danger btn-sm">Delete</button>
 			</div>
--- src/TaskListComponent.js
+++ src/TaskListComponent.js
@@ -1,13 +1,31 @@
+import { computed } from 'vue';
 import TaskItemRowComponent from './TaskItemRowComponent.js';
 
 export default {
 	components: {
 		'task-item': TaskItemRowComponent,
 	},
+	data(){
+		return {
+			editedTaskId: null,
+		}
+	},
+	provide(){
+		return {
+			editedTaskId: computed(() => this.editedTaskId),
+		}
+	},
 	props:[ 'tasks' ],
+	methods: {
+		onUpdateEditing(val){
+			if(val){
+				this.editedTaskId = val;
+			}	
+		}
+	},
 	template: /* html */`
 		<ul class="list-group">
-			<task-item v-for="task in tasks" :key="task.id" :task="task"></task-item>
+			<task-item v-for="task in tasks" @update:editing="onUpdateEditing" :key="task.id" :task="task"></task-item>
 		</ul>
 	`,
 }
